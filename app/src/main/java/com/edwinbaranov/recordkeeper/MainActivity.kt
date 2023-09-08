06ad68b386ddb691ca41a154381c7c8f05580550
Edwin Baranov <edwin.baranov@gmail.com>
2023-09-08 15:54:07 -0700

    Record Keeper init push

---
+++ app/src/main/java/com/edwinbaranov/recordkeeper/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.edwinbaranov.recordkeeper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
