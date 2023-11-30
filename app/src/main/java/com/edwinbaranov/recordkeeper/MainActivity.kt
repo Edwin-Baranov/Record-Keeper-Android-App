b01d52eaf8ba329c0262d26cfa2af2bf9ce20a6d
Edwin Baranov <edwin.baranov@gmail.com>
2023-11-30 14:20:42 -0800

    Removed testing buttons and connected bottom navigation buttons

--- app/src/main/java/com/edwinbaranov/recordkeeper/MainActivity.kt
+++ app/src/main/java/com/edwinbaranov/recordkeeper/MainActivity.kt
@@ -2,6 +2,7 @@ package com.edwinbaranov.recordkeeper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.fragment.app.commit
 import com.edwinbaranov.recordkeeper.databinding.ActivityMainBinding
 
@@ -14,21 +15,28 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.runningButton.setOnClickListener { onRunningClicked() }
-        binding.cyclingButton.setOnClickListener { onCyclingClicked() }
+        binding.bottomNav.setOnItemSelectedListener { item -> onNavigationClicked(item) }
     }
 
-    private fun onRunningClicked() {
+    private fun onRunningClicked(): Boolean {
         supportFragmentManager.commit {
             replace(R.id.frame_content, RunningFragment())
         }
+        return true;
     }
 
-    private fun onCyclingClicked() {
+    private fun onCyclingClicked(): Boolean {
         supportFragmentManager.commit {
             replace(R.id.frame_content, CyclingFragment())
         }
+        return true;
     }
 
-
+    private fun onNavigationClicked(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.nav_cycling -> onCyclingClicked()
+            R.id.nav_running -> onRunningClicked()
+            else -> false;
+        }
+    }
 }
