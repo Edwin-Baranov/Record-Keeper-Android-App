868a73284e96ae8b2dd0cfe2b20efdcdc4d9f423
Edwin Baranov <edwin.baranov@gmail.com>
2023-12-01 13:30:24 -0800

    Added edit record activity, added theme to show action bar

---
+++ app/src/main/java/com/edwinbaranov/recordkeeper/EditRunningRecordActivity.kt
@@ -0,0 +1,20 @@
+package com.edwinbaranov.recordkeeper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.edwinbaranov.recordkeeper.databinding.ActivityEditRunningRecordBinding
+
+class EditRunningRecordActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEditRunningRecordBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityEditRunningRecordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val distance = intent.getStringExtra("Distance")
+        title = "$distance Record"
+    }
+}
